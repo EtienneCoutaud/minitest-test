4219aa0a1d25c981e9ef6efaf8b54520684b0499
Etienne Coutaud <ecoutaud@octo.com>
2016-07-24 18:46:44 +0200

    add topo file

---
+++ topo-1s-2h.py
@@ -0,0 +1,32 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host1 --- switch1 --- host2
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        host1 = self.addHost( 'h1' )
+        host2 = self.addHost( 'h2' )
+        switch1 = self.addSwitch( 's1' )
+
+        # Add links
+        self.addLink( host1, switch1 )
+        self.addLink( host2, switch1 )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
